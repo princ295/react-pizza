f468af6b3e8cbc7fccbaf346fb526fb244f76b22
Prince Dewangan <dprince@BLR.ALLEGISINDIA.COM>
2024-04-19 12:19:06 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+// import Categories from "./componets/Category"
+// import Header from "./componets/Header";
+
+import Card from "./componets/Card/Card"
+import Categories from "./componets/Category/Category"
+import Header from "./componets/Header"
+import Home from "./pages/Home/Home"
+
 
 
 function App() {
-  return(
-    <></>
+  return (
+    <div className="main wrapper">
+
+      <Header />
+     <Home />
+
+      <div className="container cards">
+        {/* {
+        pizza.map((el: any, i: number) => <Card />)
+      } */}
+      </div>
+    </div>
   )
 
 }
